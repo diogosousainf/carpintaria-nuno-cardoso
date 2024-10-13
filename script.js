224d001335572b982c83ce8c0b89d9ab0a5de9ad
Diogo Sousa <diogosousainf@gmail.com>
2024-10-13 20:12:37 +0100

    1

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
 document.querySelector('.close-menu').addEventListener('click', function () {
     document.querySelector('.navbar-collapse').classList.remove('show');
 });
-document.addEventListener('DOMContentLoaded', function() {
-    var video = document.querySelector('video');
-    if (video) {
-        video.play().catch(function(error) {
-            console.log("Autoplay failed:", error);
-        });
-    }
-});
